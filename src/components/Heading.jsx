e959ee9fd7ebb213939a4e3a9e74379a606d6888
Kartikeya Sharma <kartikeyasharma@Kartikeyas-MacBook-Pro.local>
2022-08-05 17:09:30 +0530

    header and splash page added

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heading = ({text}) => {
+  return (
+    <div class="mx-5 text-6xl font-bold text-gray-800">
+            {text}
+    </div>
+  )
+}
+
+export default Heading
---
+++ src/pages/splash/SplashScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Headers from '../../components/Headers'
+import Heading from '../../components/Heading'
+
+const SplashScreen = () => 
+{
+  return (
+      <div className='mx-16 my-2'>
+         <Headers/>
+         <Heading text = "Courses"/>
+      </div>
+  
+  )
+
+}
+
+export default SplashScreen
